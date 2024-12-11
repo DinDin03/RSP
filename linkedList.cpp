fc29af5289c0b5afa7edf6918880d3e0204c031f
DinDin03 <dineth.himasha@outlook.com>
2024-12-11 14:42:43 +1030

    adding files

--- linkedList.cpp
+++ linkedList.cpp
@@ -11,40 +11,41 @@ struct Node { // Create a structure called Node which has 2 member variables dat
   }
 };
 
-class LinkedList {
+class LinkedList { // Class called linkedList 
  private:
-  Node* head;
+  Node* head; // Stores a private member called head which stores the current head of the linked list
 
  public:
-  LinkedList() { head = nullptr; }
+  LinkedList() { head = nullptr; } // Default constructor for LinkedList which sets the head to NULL. List is empty
 
-  void insertAtBeginning(int value) {
-    Node* newNode = new Node();
-    newNode->data = value;
-    newNode->next = head;
-    head = newNode;
+  void insertAtBeginning(int value) { // Function to insert a value at the beginning of the list
+    Node* newNode = new Node(); // Create a new Node called newNode
+    newNode->data = value; // Set the data of newly created newNode to the value that we pass as a parameter 
+    newNode->next = head; // Set the next node of the newNode to point to the 'current' head
+    head = newNode; // update the head to the newNode since we want to insert this at the beginning. newNode will become the head of the list
   }
 
-  void insertAtEnd(int value) {
-    Node* newNode = new Node();
-    newNode->data = value;
-    newNode->next = nullptr;
-    Node* temp = head;
-    while (temp->next != nullptr) {
-      temp = temp->next;
+  void insertAtEnd(int value) { // Function to insert a value at the end of the list
+    Node* newNode = new Node(); // Create a new node called newNode
+    newNode->data = value; // set the data of the newNode to be the value that we pass as a parameter
+    newNode->next = nullptr; // set the next pointer of the newNode to be the null pointer since we are adding it at the end of the list 
+    Node* temp = head; // create a temporary node called temp which will be the head of the node
+    while (temp->next != nullptr) { // iterate through the list until the last element before the null pointer
+      temp = temp->next; // increment. ie make the temp node point to the next node of the list
     }
-    temp->next = newNode;
+    temp->next = newNode; // since we want to insert at the end, the element before the nullpointer should now point to the newNode we created
+    delete temp; // delete temp to free memory
   }
 
-  void insertAtPosition(int value, int position) {
-    Node* newNode = new Node();
-    newNode->data = value;
-    Node* temp = head;
-    for (int i = 1; i < position - 1; i++) {
-      temp = temp->next;
+  void insertAtPosition(int value, int position) { // Function to insert a value at any poisition, takes 2 paramters one for value and one for position
+    Node* newNode = new Node(); // create a new Node called newNode
+    newNode->data = value; // make the data of the newNode be the value that we pass
+    Node* temp = head; // Create a temporary node which is set to the head of the list
+    for (int i = 1; i < position - 1; i++) { // iterate through the list until the element before the desired position
+      temp = temp->next; // Increment the temp
     }
-    newNode->next = temp->next;
-    temp->next = newNode;
+    newNode->next = temp->next; // set the next of the newNode to point to the desired poisiton
+    temp->next = newNode; // update the next of the previous position to point to the new Node
   }
 
   void deleteFromBeginning() {
